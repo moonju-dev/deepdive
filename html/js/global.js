41d8f8eb07863868f1a95e5bde14998e0b44e889
Moonju <jj2614143@gmail.com>
2025-02-04 21:09:21 +0900

    2/4일 작업

--- html/js/global.js
+++ html/js/global.js
@@ -3,3 +3,7 @@ import { includeHTML } from './include.js';
 includeHTML("/guide/heading.html", "#heading");
 includeHTML("/guide/link.html", "#link");
 includeHTML("/guide/text.html", "#text");
+includeHTML("/guide/checkbox.html", "#checkbox");
+includeHTML("/guide/radio.html", "#radio");
+includeHTML("/guide/select.html", "#select");
+includeHTML("/guide/button.html", "#button");
