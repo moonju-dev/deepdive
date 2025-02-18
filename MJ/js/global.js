c68e18e2509b044ce60fe83f096d8434e0501dcb
Moonju <jj2614143@gmail.com>
2025-02-18 20:26:41 +0900

    MJ 파일 팝업 추가 html,css

--- MJ/js/global.js
+++ MJ/js/global.js
@@ -3,3 +3,4 @@ import { includeHTML } from './include.js';
 includeHTML("/guide/heding.html", "#header");
 includeHTML("/guide/link.html", "#link");
 includeHTML("/guide/text.html", "#text");
+includeHTML("/guide/grid.html", "#grid");
