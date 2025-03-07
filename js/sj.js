1ef8aa6a304acc45edad02d63691da3fa85a1389
Sejin <sejin.han@gmail.com>
2025-03-07 21:24:18 +0900

    feat: change_reservation_1 인터렉션 추가

--- js/sj.js
+++ js/sj.js
@@ -2,23 +2,35 @@
 gsap
   .timeline()
   .from(".change .tab-type1", {
-    xPercent: 50,
+    width: 0,
     opacity: 0,
+    ease: "power1.inOut",
   })
-  .from(".change .chip", {
+  .from(".change .tab-type1 li span", {
     y: 20,
     opacity: 0,
+    stagger: 0.3,
+    ease: "power1.inOut",
+  })
+  .from(".change .chip label", {
+    y: 20,
+    opacity: 0,
+    stagger: 0.3,
+    ease: "power1.inOut",
   })
   .from(".change .list-title", {
     y: 20,
     opacity: 0,
+    ease: "power1.inOut",
   })
   .from(".list-content .date", {
     y: 20,
     opacity: 0,
+    ease: "power1.inOut",
   })
   .from(".list-content .card", {
     opacity: 0,
     y: 20,
     stagger: 0.3,
+    ease: "power1.inOut",
   });
