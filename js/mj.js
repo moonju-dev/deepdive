ac630be2f5127a312a93d8271fdd9a3695f52adc
Moonju <jj2614143@gmail.com>
2025-03-11 21:45:36 +0900

    예약변경페이지 js 추가

--- js/mj.js
+++ js/mj.js
@@ -22,3 +22,85 @@ gsap
     y: 20,
     duration: 0.5,
   });
+
+gsap.fromTo(
+  ".ani1",
+  {
+    x: -30,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".ani1",
+      //markers: true,
+      start: "top center",
+    },
+  }
+);
+
+gsap.from(".ani2 li", {
+  opacity: 0,
+  y: 40,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".ani2",
+    //markers: true,
+    start: "50px center",
+    end: "bottom center",
+    scrub: true,
+  },
+});
+
+gsap.fromTo(
+  ".ani3",
+  {
+    x: -30,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".ani3",
+      //markers: true,
+      start: "top center",
+      end: "bottom center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".ani4",
+  {
+    y: -30,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".ani4",
+      //markers: true,
+      start: "top center",
+      end: "bottom center",
+    },
+  }
+);
+
+gsap.from(".ani5", {
+  opacity: 0,
+  y: 40,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".ani5",
+    //markers: true,
+    start: "100px center",
+    end: "bottom center",
+    scrub: true,
+  },
+});
